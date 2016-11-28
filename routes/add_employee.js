6fb75f779caa48d256d92abe7ed607947898ddee
Veronica Lona <v.martinezlona@gmail.com>
2016-11-27 20:04:29 -0600

    add get, post in employee_add.js

--- routes/add_employee.js
+++ routes/add_employee.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Employee = require('../models/employee.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('add_employee');
 });
 
-/*
-router.post('/')
-*/
+
+router.post('/', function(req, res){
+  console.log(req.body);
+
+  var employee = new Employee({
+    photo: req.body.photo,
+    lastName: req.body.lastName,
+    firstName: req.body.firstName,
+    address: req.body.address,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  employee.save(function(error, employee){
+    if(error){
+      res.send(500, error.message);
+    }
+    Employee.find(function(error, employeeList){
+      if(error){
+        res.send(500, error.message);
+      }
+      res.render('employee');
+    });
+  });
+});
+
 module.exports = router;
