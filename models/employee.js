4f467787b1981d8f5ea6e5e8195e808719a745af
Veronica Lona <v.martinezlona@gmail.com>
2016-11-27 19:31:14 -0600

    Add employeeSchema

--- models/employee.js
+++ models/employee.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var employeeSchema = new Schema({
+  photo: String,
+  lastName: String,
+  firstName: String,
+  address: String,
+  username: String,
+  password: String
+});
+
+module.exports = mongoose.model('employee', employeeSchema);
